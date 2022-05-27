4e9ce2bf4f9ea394184eab36eb23cba67f8a9f6a
Anyanzor <yanilanzor@gmail.com>
2022-05-27 19:56:56 -0300

    lista.js2

---
+++ JavaScript-Basico-main/sesion-01-02-mi-primer-proyecto-de-node/lista.js
@@ -0,0 +1,8 @@
+const libro = {
+    titulo: "El Retrato de Dorian Gray",
+    autor: "Oscar Wilde",
+    año: "1890",
+    url: "https://www.culturagenial.com/es/el-retrato-de-dorian-gray-de-oscar-wilde/"
+}
+
+const lista = ["Deborah", 20, true, libro];
