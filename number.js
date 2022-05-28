f45bd7f015efcb56057736ab2dd310ff20ae8171
Anyanzor <yanilanzor@gmail.com>
2022-05-28 18:00:30 -0300

    numbers

---
+++ number.js
@@ -0,0 +1,21 @@
+let alt = 1.73;
+
+let altura = parseInt(alt);
+console.log(altura);
+
+let alturaM = parseFloat(alt);
+console.log(alturaM);
+
+let p = 55.5;
+
+let peso = parseFloat(p);
+console.log(peso);
+
+let alturaUp = (alturaM.toFixed());
+console.log(alturaUp);
+
+let pesoDown = Math.floor(p);
+console.log(pesoDown);
+
+let maxValueJS = ("el máximo valor que se puede obtener en Javascript + 1 = " + Number.MAX_VALUE);
+console.log(maxValueJS);
