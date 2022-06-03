e5d62a588740b0bff4612c9bae0175f8ecc947c0
Anyanzor <yanilanzor@gmail.com>
2022-06-03 13:50:13 -0300

    nodeProject

---
+++ JavaScript-Basico-main/sesion-10-modulos/newNodeProject/index.js
@@ -0,0 +1,8 @@
+import * as moduloMatematicas from './modulos/controller.js';
+
+const sum = moduloMatematicas.suma(4, 3)
+console.log(sum)
+
+const mul = moduloMatematicas.multiplica(2, 2)
+import chalk from 'chalk';
+console.log(chalk.green(mul));
